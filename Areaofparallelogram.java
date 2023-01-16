eef9a9fb6508809fd2ddadbdee31c9e23a3c39fe
Pavan052001 <120703366+Pavan052001@users.noreply.github.com>
2023-01-16 14:26:53 +0530

    area of parallelogram using java

---
+++ Areaofparallelogram.java
@@ -0,0 +1,13 @@
+class Areaofparallelogram
+{
+public static void main(String args[])
+{
+
+int b = 8;
+int  h = 2;
+
+  int Areaofparallelogram = b*h;
+
+System.out.println("Areaofparallelogram :"+Areaofparallelogram);
+}
+}
