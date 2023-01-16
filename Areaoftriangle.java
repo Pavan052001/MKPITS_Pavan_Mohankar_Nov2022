ae6e761fbfeb094a4a1c8e386a129f1bcdbf3bee
Pavan052001 <120703366+Pavan052001@users.noreply.github.com>
2023-01-16 14:27:59 +0530

    finding area of triangle using java

---
+++ Areaoftriangle.java
@@ -0,0 +1,13 @@
+class Areaoftriangle
+{
+public static void main(String args[])
+{
+
+double base = 14;
+double  height = 2;
+
+  double Areaoftriangle = (base*height)/2;
+
+System.out.println("Areaoftriangle :"+Areaoftriangle);
+}
+}
