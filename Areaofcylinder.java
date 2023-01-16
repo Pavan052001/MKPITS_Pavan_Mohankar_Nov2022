590ed43cd08ddfa569bd044b2dd8a8d4ac528c36
Pavan052001 <120703366+Pavan052001@users.noreply.github.com>
2023-01-16 14:26:08 +0530

    finding area of  cylinder using java

---
+++ Areaofcylinder.java
@@ -0,0 +1,13 @@
+class Areaofcylinder
+{
+public static void main(String args[])
+{
+
+double pai = 3.14;
+double  r = 2;
+double  h=5;
+  double Areaofcylinder = 2*pai*r*h+2*pai*(r*r);
+
+System.out.println("Areaofcylinder :"+Areaofcylinder);
+}
+}
