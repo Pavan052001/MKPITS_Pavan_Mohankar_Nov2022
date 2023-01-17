65298cee43f2cdcee3f94163e81a33bcab67a812
Pavan052001 <120703366+Pavan052001@users.noreply.github.com>
2023-01-17 10:19:37 +0530

    emplicit in java

---
+++ typecast_implicit.java
@@ -0,0 +1,15 @@
+public class typecast_implicit{
+
+
+public static void main(String[] args){
+
+byte a=2;
+//short s=a;
+long b=12345123456780;
+a=(byte)b;
+
+ //System.out.println("Enter the answer :"+s);
+ System.out.println("Enter the answer :"+a);
+
+}
+}
