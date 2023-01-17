b7e880eab33ea99e840ee5a234131f4b9cb282b6
Pavan052001 <120703366+Pavan052001@users.noreply.github.com>
2023-01-17 10:20:38 +0530

    class method in java

---
+++ classMethod_div.java
@@ -0,0 +1,26 @@
+class div{
+
+int a=5;
+int b=5;
+int res;
+
+
+void div()
+{
+res=a/b;
+}
+
+void dis()
+{
+System.out.println(res);
+}
+}
+
+public class classMethod_div{
+public static void main(String[] args){
+
+div ob=new div();
+ob.div();
+ob.dis();
+}
+}
