d1e04b16e40072851851a8f888cd41d497eee3a6
Pavan052001 <120703366+Pavan052001@users.noreply.github.com>
2023-01-25 10:33:37 +0530

    23  jan assignment 10

---
+++ as_10_23jan.java
@@ -0,0 +1,31 @@
+
+// 10 Write a C program to check three given integers (small, medium and large) and return true
+//if the difference between small and medium and the difference between medium and large is same.
+
+
+public class as_10_23jan{
+
+public static void main(String[] args){
+boolean check =false;
+int x,y;
+int a=2;
+int b=4;
+int c=6;
+
+ x=b-a;
+  y=c-b;
+
+if(x==y)
+{
+	check= true;
+System.out.println(check);
+
+}
+else{
+
+System.out.println(check);
+
+}
+}
+}
+
