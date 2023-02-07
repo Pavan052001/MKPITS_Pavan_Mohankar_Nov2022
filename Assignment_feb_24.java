e8ac7419adf2da7ac426e7733546093e9f52a030
Pavan052001 <120703366+Pavan052001@users.noreply.github.com>
2023-02-07 07:26:36 +0530

    Assignment_feb_24.java
    Write a program in java to read any day number in integer and display day name in the word.

---
+++ Assignment_feb_24.java
@@ -0,0 +1,32 @@
+//Write a program in C to read any day number in integer and display day name in the word.
+
+import java.util.Scanner;
+ class Assignment_feb_24 {
+
+
+  public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int day = in.nextInt();
+
+        System.out.println(getDayName(day));
+    }
+
+    // Get the name for the Week
+    public static String getDayName(int day) {
+        String dayName = "";
+        switch (day) {
+            case 1: dayName = "Monday"; break;
+            case 2: dayName = "Tuesday"; break;
+            case 3: dayName = "Wednesday"; break;
+            case 4: dayName = "Thursday"; break;
+            case 5: dayName = "Friday"; break;
+            case 6: dayName = "Saturday"; break;
+            case 7: dayName = "Sunday"; break;
+            default:dayName = "Invalid day range";
+        }
+
+        return dayName;
+    }
+}
