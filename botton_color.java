4dd297483c3de464c05ca7d643cd11906fcb2902
Pavan052001 <120703366+Pavan052001@users.noreply.github.com>
2023-02-07 07:11:22 +0530

    botton_color.java handler
    botton color using event

---
+++ botton_color.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class JcolorChooserDemo extends JFrame implements ActionListener
+{
+   JButton btn;
+   Container c;
+
+
+}
