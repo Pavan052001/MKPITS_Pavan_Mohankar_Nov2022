f10cfaad310e051118da43777ee9bd5d1f03c4ed
Pavan052001 <120703366+Pavan052001@users.noreply.github.com>
2023-02-13 21:26:13 +0530

    number reverse in java
    reverse number in java

---
+++ intreverse.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class intreverse{
+
+public static void main(String[] args){
+
+
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number");
+
+int num=sc.nextInt();
+
+int rev= 0;
+
+while(num!=0){
+
+	 rev=rev*10+num%10;
+	 num=num/10;
+
+	}
+	System.out.println(rev);
+
+
+}
+}
