ac4a4a8d6b20ce88241fa404d5088913a20db347
Sarveshbagde <sarveshbagde@gmail.com>
2023-02-28 13:55:23 +0530

    fibonacci series

---
+++ Fibonacci_Series.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+class  Fibonacci_Series{
+
+public static void main(String [] args){
+
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter number for fibonaccci series");
+	int n=sc.nextInt();
+
+int a=0,b=1,c;
+
+for(int i=0;i<n;i++){
+
+c=a+b;
+a=b;
+b=c;
+System.out.print(a + ", ");
+
+}
+
+}
+
+
+}
+
