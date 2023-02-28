ae8b86e7dbc08f0bc72ae7106da7c09acc78ead3
Sarveshbagde <sarveshbagde@gmail.com>
2023-02-28 13:56:47 +0530

    find max element of array

---
+++ find_max_element.java
@@ -0,0 +1,36 @@
+
+
+class find_max_element{
+
+public static void main(String[] args){
+
+
+int arr[]={11,16,54,32};
+
+for(int i=0;i<arr.length;i++){}
+
+if(arr[0]>arr[1] && arr[0]>arr[2]&& arr[0]>arr[3]){
+
+System.out.println("max="+arr[0]);
+}
+else if(arr[1]>arr[0]&& arr[1]>arr[2]&&arr[1]>arr[3]){
+
+	System.out.println("max="+arr[1]);
+
+	}
+	else if (arr[3]>arr[0]&& arr[3]>arr[1]&&arr[3]>arr[2]){
+
+		System.out.println("max="+arr[2]);
+
+		}
+
+        else if (arr[2]>arr[0]&& arr[2]>arr[1]&&arr[2]>arr[3]){
+
+				System.out.println("max"+arr[2]);
+
+				}
+
+
+
+}
+}
