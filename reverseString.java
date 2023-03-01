eada2e3f53fa9699f02af484bf67fa6804194cb1
Sarveshbagde <sarveshbagde@gmail.com>
2023-03-01 22:40:20 +0530

    reverse string

---
+++ reverseString.java
@@ -0,0 +1,21 @@
+
+
+class reverseString{
+
+public static void main(String[] args){
+
+String a="pavan";
+
+String str="";
+
+for(int i=a.length()-1;i>=0;i--){
+
+
+str=str+a.charAt(i);
+}
+
+System.out.println(str);
+
+}
+
+}
