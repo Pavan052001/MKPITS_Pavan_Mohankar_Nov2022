cca74a9a48cd9c816aba5e865fabaeb242aa7742
Sarveshbagde <sarveshbagde@gmail.com>
2023-03-01 22:39:15 +0530

    converting char to String in java

---
+++ Convert_charArraytostring.java
@@ -0,0 +1,18 @@
+
+//How to convert a char array to a string in Java
+
+
+
+class Convert_charArraytostring
+{
+   public static void main(String args[])
+   {
+
+      char[] ch = {'g', 'o', 'o', 'd', ' ', 'm', 'o', 'r', 'n', 'i', 'n', 'g'};
+      String str = new String(ch);
+      System.out.println(str);
+
+
+
+   }
+}
