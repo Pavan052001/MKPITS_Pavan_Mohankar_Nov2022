47a80b717e4b1692b92fea8a639388e66f35f0bc
Sarveshbagde <sarveshbagde@gmail.com>
2023-03-02 23:04:44 +0530

    remove white spaces from String

---
+++ remove_white_spaces_from_string.java
@@ -0,0 +1,12 @@
+public class remove_white_spaces_from_string {
+    public static void main(String[] args) {
+
+        String a=" hello my friend";
+
+        System.out.println("before removing white spaces : "+a);
+
+         a=a.replaceAll("\\s","");
+
+        System.out.println("after removing white spaces : "+a);
+    }
+}
