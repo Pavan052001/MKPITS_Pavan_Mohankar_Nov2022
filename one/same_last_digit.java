e1a85462f0db326a7279eb626be105f10812064c
Sarveshbagde <sarveshbagde@gmail.com>
2023-03-09 10:47:52 +0530

    image

---
+++ one/same_last_digit.java
@@ -0,0 +1,16 @@
+public class same_last_digit {
+
+    public static void main(String[] args) {
+
+        int a=123;
+        int b=143;
+
+        if(a%10==b%10){
+
+            System.out.println("same last digit");
+        }
+        else{
+            System.out.println("different last digit");
+        }
+    }
+}
